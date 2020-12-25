6b45aad7709eb95556d3b6f9e0fd9456b3fdedac
Serhii <serhii@a-dam.com>
2020-12-25 20:55:03 +0200

    ProductListItem updated with ValuesList and Image components

--- src/components/ProductListItem/ProductListItem.jsx
+++ src/components/ProductListItem/ProductListItem.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-import { PropertyList } from "../PropertyList/PropertyList";
-
-import s from "./styles/ProductListItem.scss";
 import { PROP_AS_ID } from "../../core/hooks";
 
-// propList - list of props. to compare. Same as in
+import { Image } from "../Image/Image";
+import { ValuesList } from "../ValuesList/ValuesList";
 
-export const ProductListItem = ({ productItem, propsList, valueDiffPropsList, isActive, onItemRemove }) => {
+import s from "./styles/ProductListItem.scss";
+
+export const ProductListItem = ({ productItem, propsList, isActive, onItemRemove }) => {
    return (
       <div className={`${s.productItem} ${!isActive ? s.disabled : ""}`}>
          <div className={s.header}>
             <div className={s.imageContainer}>
+               <Image path={productItem["productImage"]} />
                <div className={s.removeItemContainer}>
                   <input type="checkbox" onChange={() => onItemRemove(productItem[PROP_AS_ID])} title={"Remove"} />
                   <label>Remove</label>
@@ -21,7 +22,7 @@ export const ProductListItem = ({ productItem, propsList, valueDiffPropsList, is
             <div className={s.priceContainer}></div>
          </div>
          <div className={s.propListContainer}>
-            <PropertyList propsList={propsList} valueDiffPropsList={valueDiffPropsList} />
+            <ValuesList productItem={productItem} propsList={propsList} />
          </div>
       </div>
    );
