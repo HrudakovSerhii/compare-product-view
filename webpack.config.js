7526c4b3cf8a57bb9b438a20d4df75acd674070e
Serhii <serhii@a-dam.com>
2020-12-25 18:34:41 +0200

    CSS/SASS setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,77 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const SRC_DIR = __dirname + '/src';
-const DIST_DIR = __dirname + '/dist';
+const SRC_DIR = __dirname + "/src";
+const DIST_DIR = __dirname + "/dist";
+const devMode = true;
 
 module.exports = {
-    entry: [
-        SRC_DIR + '/index.jsx',
-    ],
-    output: {
-        path: DIST_DIR,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(html)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'html-loader',
-                    options: {minimize: true}
-                }
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: SRC_DIR + '/index.html',
-            filename: './index.html'
-        })
-    ],
-    devServer: {
-        contentBase: DIST_DIR,
-        hot: true,
-        port: 4000
-    }
+   entry: [SRC_DIR + "/index.jsx"],
+   output: {
+      path: DIST_DIR,
+      publicPath: "/",
+      filename: "bundle.js"
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(html)$/,
+            exclude: /node_modules/,
+            use: {
+               loader: "html-loader",
+               options: { minimize: true }
+            }
+         },
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader"
+            }
+         },
+         {
+            test: /\.(scss|sass|css)$/,
+            include: SRC_DIR,
+            exclude: /node_modules/,
+            use: [
+               devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+               {
+                  loader: "css-loader",
+                  options: {
+                     modules: {
+                        localIdentName: "[local]_[hash:base64:5]"
+                     },
+                     sourceMap: true
+                  }
+               },
+               {
+                  loader: "sass-loader",
+                  options: {
+                     sourceMap: true
+                  }
+               }
+            ]
+         }
+      ]
+   },
+   resolve: {
+      extensions: ["*", ".js", ".jsx"]
+   },
+   plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+         template: SRC_DIR + "/index.html",
+         filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+         filename: devMode ? "[name].css" : "[name].[hash].css",
+         chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+      })
+   ],
+   devServer: {
+      contentBase: DIST_DIR,
+      hot: true,
+      port: 4000
+   }
 };
