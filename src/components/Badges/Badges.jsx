34905ad0070ce402451afe653d5f635b854a4210
Serhii <serhii@a-dam.com>
2020-12-26 16:42:56 +0200

    Badges styles moved to component style file. Updated size of icons and paddings

--- src/components/Badges/Badges.jsx
+++ src/components/Badges/Badges.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import { Image } from "../Image/Image";
 
-import s from "../../styles/common.scss";
+import s from "./styles/Badges.scss";
 
-export const Badges = ({ data }) => {
+export const Badges = ({ data, hasDiff }) => {
    const urls = data.split("|");
 
    return urls?.length ? (
-      <div className={s.badgeList}>
+      <div className={`${s.badgeList} ${hasDiff ? s.hasDiff : ""}`}>
          {urls?.map((url, i) => (
             <Image key={url + i} path={url} style={s.badge} />
          ))}
