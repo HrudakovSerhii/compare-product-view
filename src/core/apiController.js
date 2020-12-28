95df64a79e3868c74362ccb0ef6afdac70288688
Serhii <serhii@a-dam.com>
2020-12-28 14:18:11 +0200

    fetch policy fro dev/prod modes

--- src/core/apiController.js
+++ src/core/apiController.js
@@ -1,21 +1,20 @@
-
 export const get = async (url) => {
    try {
       const response = await fetch(url, {
-         cache: 'force-cache', // Dev setup
+         cache: process.env.NODE_ENV === "development" ? "force-cache" : "default",
          headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
          }
       });
 
       return {
          data: await response?.json(),
          errors: undefined
-      }
+      };
    } catch (e) {
       return {
          data: undefined,
          errors: e
-      }
+      };
    }
-}
+};
